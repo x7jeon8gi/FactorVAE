c9960f53c52d90d17dfa49c14804f7256b8b82bd
gi8jeon7 <40204613+x7jeon8gi@users.noreply.github.com>
2023-04-13 22:50:38 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -20,15 +20,24 @@ class StockDataset(torch.utils.data.Dataset):
         self.groups = df.groupby('datetime')
         self.rows_to_keep = []
         for _, group in self.groups:
+            # drop row if their LABEL0 is NaN
+            group = group.dropna(subset=['LABEL0'])
             if len(group) < 300:
                 new_group = pd.DataFrame(np.nan, index=np.arange(300), columns=df.columns)
                 new_group.iloc[:len(group), :] = group.values
+                
+                # sort by LABEL0
                 new_group = new_group.sort_values(by='LABEL0', ascending=False)
                 new_group = new_group.fillna(0)
+                
+                # ignore index
+                new_group = new_group.reset_index(drop=True)
                 self.rows_to_keep.append(new_group)
+                assert new_group['LABEL0'].isnull().sum() == 0, 'NaN values should not be present in the dataframe'
             else:
                 self.rows_to_keep.append(group)
-        self.df = pd.concat(self.rows_to_keep)
+        self.df = pd.concat(self.rows_to_keep, ignore_index=True)
+        assert self.df['LABEL0'].isnull().sum() == 0, 'LABEL0 column should not contain any NaN values'
         self.input_size = 30 * len(self.df.columns)  # calculate input size based on 30 days of data
 
     def __len__(self):
